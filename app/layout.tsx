77e88e675014adb6182cc2955a1eee36accea8c0
Isaladwy <isaladwy@gmail.com>
2025-01-20 19:59:14 +0200

    Fix import path for global styles in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import '@app/global.css';
+import './globals.css';
 import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '../lib/constants';
 
 
